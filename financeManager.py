603963f92e671f95c785512fe06d78a79f2bb519
unmenore <tatarskij.d@yandex.ru>
2022-08-23 11:20:41 +0300

    Add catefory name

--- financeManager.py
+++ financeManager.py
@@ -1,25 +1,32 @@
 import csv
-from operator import delitem
-from sre_constants import CATEGORY
 #import gspread
 
 MONTH = 'august'
 
 file = f"sber_{MONTH}.csv"
 
-transaction = []
+transactions = []
 
 with open(file, mode='r', encoding='utf-8') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter = ',', quotechar='"')
-        for row in csv_reader:
-            if row:
-                DATE = row[1]
-                NAME = row[3]
-                AMOUNT = row[4]
-                CURRENCY = row[5]
-                CATEGORY = 'other'
-                transaction = ((DATE, NAME, AMOUNT, CURRENCY, CATEGORY ))
-                print(transaction)
+    csv_reader = csv.reader(csv_file, delimiter = ',', quotechar='"')
+    for row in csv_reader:
+        if row:
+            DATE = row[1]
+            NAME = row[3]
+            AMOUNT = row[4]
+            CURRENCY = row[5]
+            CATEGORY = 'other'
+            if NAME == "Супермаркеты":
+                CATEGORY = 'Eat'
+            if NAME == "Другим людям":
+                CATEGORY = 'Money transfer'
+            if NAME == 'ЖКХ, связь, интернет':
+                CATEGORY = 'Internet'
+            if NAME == 'Образование':
+                CATEGORY = 'Book or Education'
+            transaction = ((DATE, NAME, AMOUNT, CURRENCY, CATEGORY))
+            print(transaction)
+            transactions.append(transaction)
             
 
 #sa = gspread.service_account()
